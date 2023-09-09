db498c07135a649257fff173785c5e5d3d4afb01
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-09-09 11:30:13 -0300

    feat: inicio primeiro projeto api_rest by quarkus

---
+++ src/test/java/io/meupacote_um/InicialRecursoIT.java
@@ -0,0 +1,8 @@
+package io.meupacote_um;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class InicialRecursoIT extends InicialRecursoTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ src/test/java/io/meupacote_um/InicialRecursoTest.java
@@ -0,0 +1,21 @@
+package io.meupacote_um;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class InicialRecursoTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
